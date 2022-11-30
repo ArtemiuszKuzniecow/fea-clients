3de403107ba315d652cc9018443525c4f1f35502
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-11-30 20:36:34 +0300

    last change

--- src/components/common/Button/MyButton.jsx
+++ src/components/common/Button/MyButton.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import style from "./MyButton.module.scss";
 import PropTypes from "prop-types";
 
-const MyButton = ({ text, isDisabled, onClick, onSubmit, type }) => {
+const MyButton = ({ text, isDisabled, onClick, type }) => {
   return (
     <button
       className={style.myButton}
       disabled={isDisabled}
       onClick={onClick}
       type={type}
-      onSubmit={onSubmit}
     >
       {text}
     </button>
@@ -21,7 +20,6 @@ MyButton.propTypes = {
   type: PropTypes.string,
   isDisabled: PropTypes.bool,
   onClick: PropTypes.func,
-  onSubmit: PropTypes.func,
 };
 
 export default MyButton;
