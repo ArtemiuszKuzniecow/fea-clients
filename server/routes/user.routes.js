108dab1cf1db03701af12fb62b38b88113cd2ba3
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-12-05 19:46:58 +0300

    update

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,21 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (userId) {
+      const user = await User.findById(userId);
+      res.status(200).send(user);
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Something was wrong, try it later.",
+    });
+  }
+});
 
 module.exports = router;
